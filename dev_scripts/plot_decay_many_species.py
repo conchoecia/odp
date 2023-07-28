c96149af22d09b2eb159e4d99fcb833ce02a5f04
conchoecia <darrin.schultz91@gmail.com>
2023-07-28 20:14:44 +0200

    incremental changes

--- dev_scripts/plot_decay_many_species.py
+++ dev_scripts/plot_decay_many_species.py
@@ -72,15 +72,15 @@ def plot_decay_log(ax, df_dict):
         # make a line plot of each line
         ax.plot(df[x_column], df[y_column], label = thisfile,
                  alpha=0.25, color ="black", lw = 0.5)
-    
+
         # Add labels and legend
         ax.set_xlabel("ALG size (total genes)")
         ax.set_ylabel("fraction conserved (log2 scale)")
-    
+
         # Using log scale for x-axis
         # plt.xscale('log')
         ax.set_yscale('log', base=2)
-    return ax    
+    return ax
 
 def filelist_to_plot_data_structure(filelist, bins):
     """
@@ -103,7 +103,7 @@ def filelist_to_plot_data_structure(filelist, bins):
 
     # convert the number of bins into a set of min cutoff values scaled between 0 and 1
     min_cutoffs = [x / bins for x in range(bins)]
-    
+
     # The data structure is like so:
     # plot_these = {  (0,    0.33):   {"file1": df, "file2": df},
     #                 (0.33, 0.66):   {},
@@ -133,11 +133,11 @@ def filelist_to_plot_data_structure(filelist, bins):
             if thisALG not in thisdf["ALG"].tolist():
                 thisdf = thisdf.append({"ALG": thisALG, "total": ALG_sizes[thisALG]}, ignore_index=True)
                 print_now = True
-        
+
         # assert that the length of the dataframe is the length of the ALG_sizes dictionary
         if not len(thisdf) == len(ALG_sizes):
             raise Exception("The length of the dataframe is not the same as the length of the ALG_sizes dictionary. thisfile: {}".format(thisfile))
-        
+
         # now we add a new column of the ALG_size from the dictionary
         thisdf["ALG_size_all"] = thisdf["ALG"].map(ALG_sizes)
 
@@ -177,13 +177,13 @@ def parse_all_ALGs_to_size_df(filelist):
     """
     Read in dataframes from a list of files.
        and returns a dataframe of all of the ALGs in the dataset.
-    
+
     The thing that is returned is a dictionary where the keys are the
      ALG ids, and the values are the size of that ALG.
 
     For now we calculate the size of the ALG by finding its max value
      in all of the dataframes. In the future it would be better to just
-     record it in the decay .tsv files when we save them. 
+     record it in the decay .tsv files when we save them.
     """
     # make a dictionary of all of the ALGs and their sizes
     alg_to_size_dict = {}
@@ -203,8 +203,13 @@ def parse_all_ALGs_to_size_df(filelist):
     return alg_to_size_dict
 
 def plot_decay(ax, df_dict, y_column="diff_from_median", x_column = "total",
+               alg_to_size_dict = {},
+               plot_title = "",
                ymin = -1, ymax = 1,
-               x_axis_label = ""):
+               y_axis_label = "y_axis_label_me",
+               x_axis_label = "x_axis_label_me",
+               x_axis_labels_on = True,
+               plot_bars = False):
     """
     makes a plot of the decay values, with the values based on difference from median.
 
@@ -212,6 +217,8 @@ def plot_decay(ax, df_dict, y_column="diff_from_median", x_column = "total",
     """
     # Extract two columns from the dataframe
 
+    # keep this dummy variable to use later
+    df = None
     # Loop through each line and plot
     for thisfile in df_dict:
         df = df_dict[thisfile]
@@ -220,17 +227,52 @@ def plot_decay(ax, df_dict, y_column="diff_from_median", x_column = "total",
         # make a line plot of each line
         ax.plot(df[x_column], df[y_column], label = thisfile,
                  alpha=0.25, color ="black", lw = 1.0)
-    
+
         # Add labels and legend
         ax.set_xlabel('ALG size (total genes)')
         ax.set_ylabel('difference from median of fraction conserved')
 
         # set the xaxis label
         ax.set_xlabel(x_axis_label)
-    
+        # set the yaxis label
+        ax.set_ylabel(y_axis_label)
+
+    if plot_bars:
+        if len(alg_to_size_dict) == 0:
+            raise Exception("You must provide a dictionary of ALG ids to sizes if you want to plot bars.")
+        # In this scenario we want to plot a bar graph as a visual aide
+        ax2 = ax.twinx()  # instantiate a second axes that shares the same x-axis
+        bar_ymin = 0
+        bar_ymax = max(alg_to_size_dict.values()) * 1.05
+        ax2.set_ylim([bar_ymin, bar_ymax])
+
+        color = 'tab:blue'
+        ax2.set_ylabel('ALG size', color=color)  # we already handled the x-label with ax1
+        x_vals = []
+        y_vals = []
+        xvaldict = dict(zip(df["ALG"], df[x_column]))
+        for thisALG in alg_to_size_dict:
+            x_vals.append(xvaldict[thisALG])
+            y_vals.append(alg_to_size_dict[thisALG])
+        ## make a bar plot of the x and y values
+        #ax2.bar(x_vals, y_vals, alpha = 0.5)
+        # make a plot with single unconnected blue dots
+        ax2.plot(x_vals, y_vals, 'o', color=color, alpha = 0.33)
+        # make a vertical line up to the dots
+        for i in range(len(x_vals)):
+            ax2.plot([x_vals[i], x_vals[i]], [0, y_vals[i]], color=color, alpha = 0.33)
+        ax2.tick_params(axis='y', labelcolor=color)
+
+    # turn off the text of the xaxis if we must
+    if not x_axis_labels_on:
+        ax.set_xticklabels([])
+
     ax.set_ylim(ymin, ymax)
 
-    return ax    
+    # set the titles for this ax object
+    ax.set_title(plot_title)
+
+    return ax
 
 def fig1(plot_these = {}, index_to_bin = []):
     """
@@ -250,6 +292,9 @@ def fig1(plot_these = {}, index_to_bin = []):
 
     # now we make the individual plots for each cutoff
     for i in range(NUMBER_OF_BINS-1, -1, -1):
+        # if we're not on the first round, add some vertical space
+        if i < NUMBER_OF_BINS-1:
+            fig.subplots_adjust(hspace=0.5)
         print("We are in bin {}. Bin values are {}".format(i, index_to_bin[i]))
         plotdfs = plot_these[index_to_bin[i]]
         if len(plotdfs) > 0:
@@ -262,10 +307,85 @@ def fig1(plot_these = {}, index_to_bin = []):
 
     # Save the plot as a jpeg
     plt.savefig('output_plot.jpg', format='jpeg')
-    
-    # Show the plot (optional, you can comment this line if you don't want to display the plot)
-    plt.show()
 
+    # Show the plot (optional, you can comment this line if you don't want to display the plot)
+    #plt.show()
+
+def fig2(plot_these = {}, index_to_bin = [],  alg_to_size_dict = {}):
+    """
+    Make a single plot with the final format that we want in a figure.
+
+    This is basically a copy of fig1, but with the final formatting.
+    """
+    # make sure the len of alg_to_size_dict isn't zero
+    if len(alg_to_size_dict) == 0:
+        raise Exception("You must provide a dictionary of ALG ids to sizes if you want to plot bars.")
+
+    # now we plot the results as many lines
+    # use matplotlib plt
+
+    # Each column of plotting is a single bin with a minimum and maximum cutoff for the ALG conservation values
+    # We must label each column with the bins.
+    # COORDINATE SYSTEM:
+    #  x-axis: Bins in decreasing value, can access
+    NUMBER_OF_BINS = len(index_to_bin)
+    NUMBER_OF_FIGS = 3
+    fig, axes = plt.subplots(NUMBER_OF_FIGS, NUMBER_OF_BINS, figsize = (5 * NUMBER_OF_BINS, 4*NUMBER_OF_FIGS))
+    fig.suptitle('% of orthologs conserved (median) on bilaterian-cnidarian-sponge ALGs in many animals.')
+
+    # now we make the individual plots for each cutoff
+    for i in range(NUMBER_OF_BINS-1, -1, -1):
+        # add some vertical and horizontal space between the plots
+        fig.subplots_adjust(hspace=0.5)
+        fig.subplots_adjust(wspace=0.5)
+
+        print("We are in bin {}. Bin values are {}".format(i, index_to_bin[i]))
+        plotdfs = plot_these[index_to_bin[i]]
+        if len(plotdfs) > 0:
+            axes[0, i] = plot_decay( axes[0, i], plotdfs, y_column = "fraction_conserved", x_column = "ALG_size_all",
+                                    y_axis_label = "fraction conserved on ALG",
+                                    x_axis_label = "ALG size based on genes found in blast",
+                                    plot_title = "{}%-{}% conserved".format(index_to_bin[i][0], index_to_bin[i][1]),
+                                    alg_to_size_dict = alg_to_size_dict,
+                                    plot_bars = True,
+                                    ymin = -0.05, ymax = 1.05)
+            axes[1, i] = plot_decay( axes[1, i], plotdfs, y_column = "fraction_conserved", x_column = "rank",
+                                    y_axis_label = "fraction conserved on ALG",
+                                    x_axis_label = "Rank of absolute ALG size, smallest to largest",
+                                    plot_title = "{}%-{}% conserved".format(index_to_bin[i][0], index_to_bin[i][1]),
+                                    alg_to_size_dict = alg_to_size_dict,
+                                    plot_bars = True,
+                                    x_axis_labels_on = True,
+                                    x_axis_labels_are_ALGs = True,
+                                    ymin = -0.05, ymax = 1.05)
+            # for the last axis object, we want to use that space to add a legend of the file names for this bin
+            #  we will use the axes[2, i] object to do this
+            text_to_plot = list(sorted(plotdfs.keys()))
+            remove_these = ["_ALG_decay.tsv", "BCnS_LGs_"]
+            for j in range(len(text_to_plot)):
+                for thisscrub in remove_these:
+                    text_to_plot[j] = text_to_plot[j].replace(thisscrub, "")
+            text_ymin = 1
+            for j in range(len(text_to_plot)):
+                # the text position needs to change in each loop
+                text_position = (0.0, 1.0 - (j * 0.06))
+                text_ymin = text_position[1]
+                axes[2, i].text(text_position[0], text_position[1], text_to_plot[j], transform=axes[2, i].transAxes)
+            # Plot title is the species that have occurred in this bin
+            axes[2, i].set_title("Species in this bin")
+
+            # set the y-axis min and max now
+            axes[2, i].set_ylim(text_ymin - 0.05, 1.05)
+            ## now turn off the lines and ticks for this axis
+            axes[2, i].axis('off')
+
+
+    # Save the plot as a jpeg
+    plt.savefig('decay_plots_final.jpg', format='jpeg')
+    plt.savefig('decay_plots_final.pdf', format='pdf')
+
+    # Show the plot (optional, you can comment this line if you don't want to display the plot)
+    #plt.show()
 
 def main():
     # parse the arguments
@@ -275,6 +395,7 @@ def main():
     filelist = [os.path.abspath(os.path.join(args.directory, f))
                 for f in os.listdir(args.directory)
                 if f.endswith(".tsv")]
+    alg_to_size_dict = parse_all_ALGs_to_size_df(filelist)
 
     NUMBER_OF_BINS = 5
     plot_these = filelist_to_plot_data_structure(filelist, NUMBER_OF_BINS)
@@ -284,6 +405,8 @@ def main():
 
     # make the exploratory
     fig1(plot_these, index_to_bin)
+    fig2(plot_these, index_to_bin, alg_to_size_dict)
+
 
 if __name__ == "__main__":
     main()
