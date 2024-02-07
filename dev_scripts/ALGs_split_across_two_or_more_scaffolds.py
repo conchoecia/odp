04eba1710db44c502a9a93b1aaf7c00e691977d2
conchoecia <darrin.schultz91@gmail.com>
2024-02-07 13:46:35 +0100

    updating the module that parses rbh files

--- dev_scripts/ALGs_split_across_two_or_more_scaffolds.py
+++ dev_scripts/ALGs_split_across_two_or_more_scaffolds.py
@@ -22,15 +22,180 @@ Usage instructions:
   - See https://github.com/conchoecia/odp#getting-started
 """
 
+# odp stuff to format the plot
+import os
+import sys
+# ODP-specific imports
+thisfile_path = os.path.dirname(os.path.realpath(__file__))
+scripts_path = os.path.join(thisfile_path, "../scripts")
+sys.path.insert(1, scripts_path)
+import odp_plotting_functions as odp_plot
+import rbh_tools
 import argparse
+import pandas as pd
+
+# matplotlib stuff
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
 
 def parse_args():
     """
-    The things
+    The things we need to know are:
+      - -d --rbh_directory - the directory of the rbh files that we will look at
+      - -m --minsig        - the number of the minimum significance value for the whole_FET column in the rbh files. This is used to filter the rbh files.
+      - -a --alg           - the name of the ALG set that we are looking at. This should be something that is the header of the rbh files, for example "BCnSSimakov2022"
     """
+    parser = argparse.ArgumentParser(description="This program takes in a directory of .rbh files and makes a datastructure of all of the significantly-occurring gene groups on different chromosomes.")
+    parser.add_argument("-d", "--rbh_directory", help="The directory of the rbh files that we will look at")
+    parser.add_argument("-m", "--minsig", type = float, default = 0.005, help="The minimum significance value for the whole_FET column in the rbh files. This is used to filter the rbh files.")
+    parser.add_argument("-a", "--alg", help="The name of the ALG set that we are looking at. This should be something that is the header of the rbh files, for example 'BCnSSimakov2022'")
+    args = parser.parse_args()
+
+    # Check that the directory exists
+    if not os.path.exists(args.rbh_directory):
+        print("The directory you provided does not exist.")
+    return args
+
+def plot_chrom_number_vs_number_ALGs_split(ax, splitsdf, min_splits, inferredchromsize):
+    """
+    Saves a pdf of the plot of the number of ALGs split across two or more scaffolds vs the number of chromosomes.
+    returns an axis
+    """
+    # we need to go through and find all the samples that have at least min_splits
+    # first we groupby the sample
+    gb = splitsdf.groupby("sample")
+    entries = []
+    for name, group in gb:
+        # samplename
+        samplename = group["sample"].unique()[0]
+        # get the number of ALGs that are significant at all
+        present_ALG_num = len(group["gene_group"].unique())
+        # num_ALGs on at least n scaffolds
+        num_ALGs = len([x for x in group["gene_group"].value_counts() if x >= min_splits])
+        entries.append({"sample": samplename,
+                        "num_chroms": inferredchromsize[samplename],
+                        "num_ALGs": present_ALG_num,
+                        "num_ALGs_min_splits": num_ALGs})
+    # make a dataframe of all of the entries
+    df = pd.DataFrame(entries)
+    print(df)
+    # for now make a simple scatter plot
+    ax.scatter(df["num_chroms"], df["num_ALGs_min_splits"], alpha = 0.1, lw = 0)
+    ax.set_xlabel("Number of chromosomes")
+    ax.set_ylabel(f"Number of ALGs split across {min_splits} or more scaffolds")
+    return ax
+
+def plot_chrom_number_vs_number_ALGs_perchrom(ax, splitsdf, inferredchromsize):
+    """
+    For every genome, plots the number of chromosomes (x) vs the number of ALGs on each chromosome (y)
+    For every genome, we will plot every genome.
+    """
+    # we need to go through and find all the samples that have at least min_splits
+    # first we groupby the sample
+    gb = splitsdf.groupby("sample")
+    entries = []
+    for name, group in gb:
+        # samplename
+        samplename = group["sample"].unique()[0]
+        # scafcounts
+        scafcounts = group["scaffold"].value_counts()
+        for chrom in scafcounts.index:
+            entries.append({"sample": samplename,
+                            "chrom": chrom,
+                            "num_chroms": inferredchromsize[samplename],
+                            "ALGs_on_chrom": scafcounts[chrom]})
+    # make a dataframe of all of the entries
+    df = pd.DataFrame(entries)
+    #ax.scatter(df["num_chroms"], df["ALGs_on_chrom"], alpha = 0.01, lw = 0)
+    #ax.set_xlabel("Number of chromosomes")
+    #ax.set_ylabel(f"Number of ALGs on each chromosome")
+    # I don't like this plot.
+
+    # We do a little more processing, instead try plotting the mean number of ALGs on each chromosome
+    entries = []
+    gb = df.groupby(["sample"])
+    for name, group in gb:
+        entries.append({"sample": name,
+                        "num_chroms": group["num_chroms"].unique()[0],
+                        "mean_ALGs_on_chrom": group["ALGs_on_chrom"].mean()})
+    df = pd.DataFrame(entries)
+    print(df)
+
+    # for now make a simple scatter plot
+    ax.scatter(df["num_chroms"], df["mean_ALGs_on_chrom"], alpha = 0.1, lw = 0)
+    ax.set_xlabel("Number of chromosomes")
+    ax.set_ylabel(f"Mean number of ALGs on each chromosome")
+    return ax
 
 def main():
     args = parse_args()
+
+    rbh_files = [os.path.join(args.rbh_directory, f) for f in os.listdir(args.rbh_directory) if f.endswith(".rbh")]
+    # for testing purposes, just get the top 100 files
+    #rbh_files = rbh_files[100]
+    #rbh_files = [x for x in rbh_files if "Lepisosteus" in x]
+
+    # we need something of sample to chromnum
+    sample_to_chromnum = {}
+    # now we go through the files
+    entries = []
+    for i in range(len(rbh_files)):
+        # make a counter that goes back to the beginning of the line
+        print(f"\r  analyzing {i+1}/{len(rbh_files)}", end = "")
+        rbhfile = rbh_files[i]
+        rbhdf = rbh_tools.parse_rbh(rbhfile)
+        splitdf, samplename = rbh_tools.rbhdf_to_alglocdf(rbhdf, args.minsig, args.alg)
+        chromnum = rbh_tools.rbh_to_scafnum(rbhdf, samplename)
+        sample_to_chromnum[samplename] = chromnum
+        entries.append(splitdf)
+    print()
+    print(f"\r  Done analyzing {len(rbh_files)}/{len(rbh_files)}", end = "")
+
+    # make a dataframe of all of the entries
+    splitsdf = pd.concat(entries)
+
+    # make a plot
+    # CALL THIS TO GET THE VISUAL STYLE WE NEED
+    odp_plot.format_matplotlib()
+    fw = 10
+    fh = 20
+
+    fig = plt.figure(figsize=(fw, fh))
+    axes = []
+
+    #for aligning all the panels
+    left1   = 0.6
+    left2   = 6.5
+    left3   = 12.5
+    left4   = 18.5
+
+    axes = []
+
+    # This panel is the number of chromosomes vs the number of changes
+    bottom1 = 0.6
+    bottom2 = 7
+    paneldim  = 5
+    # we start with a single panel
+    plot_params = [left1    /fw, # left offset
+                   bottom1  /fh, # bottom offset
+                   paneldim /fw, # width
+                   paneldim /fh] # height
+    axes.append(fig.add_axes(plot_params))
+    axes[-1] = plot_chrom_number_vs_number_ALGs_split(axes[-1], splitsdf,
+                                                      2, sample_to_chromnum)
+
+    # This panel is the number of chromosomes vs the number of ALGs on each chromosome
+    paneldim  = 5
+    # we start with a single panel
+    plot_params = [left1   /fw, # left offset
+                   bottom2 /fh, # bottom offset
+                   paneldim/fw, # width
+                   paneldim/fh] # height
+    axes.append(fig.add_axes(plot_params))
+    axes[-1] = plot_chrom_number_vs_number_ALGs_perchrom(axes[-1], splitsdf, sample_to_chromnum)
+
+    outfilename = "ALGs_split_across_two_or_more_scaffolds.pdf"
+    fig.savefig(outfilename, bbox_inches="tight")
 
 if __name__ == "__main__":
     main()
---
+++ dev_scripts/rbh_tools.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+"""
+Program  : rbh_tools.py
+Language : python
+Date     : 2024-02-07
+Author   : Darrin T. Schultz
+Email    : darrin.schultz@univie.ac.at
+Github   : https://github.com/conchoecia/odp
+Support  : For issues or questions, please search if the topic has been discussed already
+           on github and open a new issue if not: https://github.com/conchoecia/odp/issues
+License  : GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007. See the LICENSE file.
+Citation : If you use this software for your scientific publication, please cite:
+           Schultz, DT; Haddock, SHD; Bredeson, JV; Green, RE; Simakov, O & Rokhsar, DS
+           Ancient gene linkages support ctenophores as sister to other animals. Nature (2023).
+           https://doi.org/10.1038/s41586-023-05936-6
+
+Description:
+  This program is part of the Oxford Dot Plot (odp) package on github.
+  This file contains a variety of functions that are useful for parsing and working with Reciprocal Best Hits (RBH) files.
+  The original reason this file was written was to parse a single .rbh file into something that lists which ALGs are significantly
+   located on which chromosomes.
+
+Usage instructions:
+  - See https://github.com/conchoecia/odp#getting-started
+"""
+
+import os
+import pandas as pd
+import sys
+
+def hex_color_legal(hexstr) -> bool:
+    """
+    Checks if the hex color is legal.
+    """
+    if len(hexstr) != 7:
+        return False
+    if hexstr[0] != "#":
+        return False
+    for char in hexstr[1:]:
+        if char not in "0123456789abcdefABCDEF":
+            return False
+    return True
+
+def parse_rbh(rbhfilepath) -> pd.DataFrame:
+    """
+    Parses an rbh file into a pandas dataframe.
+    Performs some checks to make sure that the file is legal.
+      - Starts by checking that the file exists.
+      - Check that there is a 'rbh' column
+      - Continues to check that all the samples have a "_scaf", "_gene", and "_pos" column.
+    """
+    # first check that the rbhfilepath exists
+    if not os.path.exists(rbhfilepath):
+        raise IOError(f"The file {rbhfilepath} does not exist.")
+    # now we read in the file
+    df = pd.read_csv(rbhfilepath, sep = "\t")
+
+    # Check that there is an rbh column
+    if "rbh" not in df.columns:
+        raise IOError(f"The rbh file, {rbhfilepath} does not have a column named 'rbh'")
+
+    samples = [x.split("_")[0] for x in df.columns if "_scaf" in x]
+    # Now check that all of the samples have a _scaf, _gene, and _pos column
+    missing_fields = []
+    for thissample in samples:
+        for suffix in ["_scaf", "_gene", "_pos"]:
+            thiscol = f"{thissample}{suffix}"
+            if thiscol not in df.columns:
+                missing_fields.append(thiscol)
+    # print an error if there are missing fields for a sample
+    if len(missing_fields) > 0:
+        raise IOError(f"Error: The following columns are missing from the rbh file, {rbhfilepath} {missing_fields}. Exiting.")
+
+    # check that all the hex colors are legal, if present in the "color" column
+    if "color" in df.columns:
+        for color in [x for x in df["color"] if pd.notna(x) ]:
+            if not hex_color_legal(color):
+                raise IOError(f"Error: The color {color} is not a legal hex color. Exiting.")
+    # If we have not raised an error, then we return the dataframe
+    return df
+
+
+def rbh_to_scafnum(df, samplename) -> int:
+    """
+    Looks in an rbh file and returns the number of scaffolds in the file.
+    If you know that the assembly only has chromosomes, this is a way that probably returns the chromosome number.
+    """
+    scafcol = f"{samplename}_scaf"
+    if scafcol not in df.columns:
+        raise IOError(f"The rbh file does not have a column named {scafcol}. Exiting.")
+    return df[scafcol].nunique()
+
+def rbhdf_to_alglocdf(df, minsig, ALGname) -> (pd.DataFrame, str):
+    """
+    This takes a .rbh filepath and returns a dataframe of the ALGs and their locations.
+
+    Returns:
+      - a tuple that contains the dataframe of the ALG coloc significance, and the sample name
+      - (df, samplename)
+    """
+    # check that the user is inputting a dataframe. If not, they are likely inputting a filepath.
+    if not isinstance(df, pd.DataFrame):
+        raise IOError(f"The input to rbhdf_to_alglocdf should be a pandas dataframe. You input a {type(df)}. Did you inadvertently input a filepath?")
+
+    # make sure that the genegroup column is present
+    if "gene_group" not in df.columns:
+        raise IOError(f"The rbh file, {rbhfilepath} does not have a column named 'gene_group'")
+
+    # we need to get the sample names
+    samples = [x.split("_")[0] for x in df.columns if "_scaf" in x]
+    # We need to check that the ALGname is in the samples
+    if not ALGname in samples:
+        raise IOError(f"The ALGname, {ALGname} is not in the samples. Exiting.")
+    # We need to check that the ALGname is not a part of the other sample names.
+    # For example, the ALGname like BCnSSimakov2022 would be a part of a samplename BCnSSimakov2022Hydra
+    othersamples = [x for x in samples if ALGname != x]
+    for othersample in othersamples:
+        if ALGname in othersample:
+            em =  f"The ALGname, {ALGname} is a part of the samplename {othersample}."
+            em += f" You shouldn't have any name overlaps. Exiting."
+            raise IOError(em)
+    # Right now, we are working on the assumption that we are looking specifically at the localization
+    #  of a single sample with a single ALG set. There should not be more than one sample left over in
+    #  othersamples.
+    if len(othersamples) > 1:
+        rm =  f"Error: There is more than one sample in the rbh file aside from the ALGname, {ALGname}."
+        rm += f" The intended usage of this function is to use the output of the odp pipeline and"
+        rm += f"  the config.yaml option: `plot_LGs: True`."
+        rm += f" The rbh files you are looking for for this function will be in the directory odp/step2-figures/ALG-species_plots/ ."
+        rm += f" Exiting."
+        raise IOError(rm)
+
+    # now we should get the sample name, it is the prefix to _scaf in the column that isn't the ALG's _scaf column
+    samplename = othersamples[0]
+    samplescafcol = f"{samplename}_scaf"
+    # use chromnum as the length of the unique entries in the samplescafcol
+    chromnum = len(df[samplescafcol].unique())
+    # get all the rows for which whole_FET is leq than the minimum sig value
+    tempdf = df[df["whole_FET"] <= minsig]
+    # First we check that grouping the "gene_group" and samplescafcol should have the same whole_FET value for each row.
+    # If not, this indicates that the Fisher's exact test was not being performed correctly.
+    db = tempdf.groupby(["gene_group", samplescafcol])
+    for name, group in db:
+        if len(group["whole_FET"].unique()) != 1:
+            raise IOError(f"The whole_FET column should have the same value for all rows in the same gene_group and {samplescafcol}. Exiting.")
+    # Everything is fine, so we can groupby all three at the same time.
+    # groupby the gene_group and the samplescafcol
+    gb = tempdf.groupby(["gene_group", samplescafcol, "whole_FET"])
+    # print the name of all the groups
+    entries = []
+    for name, group in gb:
+        entries.append({"sample": samplename,
+                        "gene_group": name[0],
+                        "scaffold": name[1],
+                        "pvalue": name[2],
+                        "num_genes": len(group),
+                        "frac_of_this_ALG_on_this_scaffold": len(group)/len(df[df["gene_group"] == name[0]])
+                        })
+    splitsdf = pd.DataFrame(entries)
+    return splitsdf, samplename
